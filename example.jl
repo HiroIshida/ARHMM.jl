f518578adb24996778297495a422bf5b9d92434e
HiroIshida <spitfire.docomo@gmail.com>
2021-09-21 07:21:59 +0900

    Plot

--- example.jl
+++ example.jl
@@ -29,18 +29,25 @@ prop_list = [prop1, prop2]
 A = [0.85 0.15;
      0.15 0.85]
 
-xs, zs = data_generation(200, A, prop_list)
-A_pred_init = [0.5 0.5;
-               0.5 0.5]
+xs, zs = data_generation(300, A, prop_list)
+A_pred_init = [0.9 0.1;
+               0.1 0.9]
 prop1_init = LinearPropagator(Diagonal([1.2]), Diagonal([0.1^2]), [0.2])
-prop2_init = LinearPropagator(Diagonal([1.5]), Diagonal([0.1^2]), [-0.2])
+prop2_init = LinearPropagator(Diagonal([1.1]), Diagonal([0.1^2]), [-0.2])
 prop_list_init = [prop1_init, prop2_init]
 mp = ModelParameters(1, A_pred_init, prop_list_init)
 
 z_ests = nothing
-for _ in 1:20 # if more than 2, usually likelihood will be static
+for _ in 1:60 # if more than 2, usually likelihood will be static
     global z_ests
     z_ests, zz_ests, log_likeli = compute_hidden_states(mp, xs)
     update_model_parameters!(mp, z_ests, zz_ests, xs)
-    println(mp.prop_list)
+    println(log_likeli)
 end
+z_preds = [argmax(z) for z in z_ests]
+
+using Plots
+T = 100
+plot(zs[1:T])
+plot!(z_preds[1:T])
+
