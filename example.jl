b966a79a7b4cd562d89e905e09bc744805310906
HiroIshida <spitfire.docomo@gmail.com>
2021-09-22 05:30:23 +0900

    Check that argumented state [x_t, x_tt] (2-order ARHMM) is necessarly

--- example.jl
+++ example.jl
@@ -8,18 +8,25 @@ using Distributions
 
 Random.seed!(0)
 states_list, phases_list = create_dataset(80)
-index = 8
+index = 22
 
+dim = 4
 seq = states_list[index]
-xs = [SVector{4, Float64}(vcat(seq[t].x, seq[t+1].x)) for t in 1:length(seq)-1]
+if dim==2
+    xs = [SVector{dim, Float64}(seq[t].x) for t in 1:length(seq)]
+elseif dim==4
+    xs = [SVector{dim, Float64}(vcat(seq[t].x, seq[t+1].x)) for t in 1:length(seq)-1]
+else
+    xs = [SVector{dim, Float64}(vcat(seq[t].x, seq[t+1].x, seq[t+2].x)) for t in 1:length(seq)-2]
+end
 zs = phases_list[index]
 
-prop1 = LinearPropagator(Diagonal(ones(4)), Diagonal(ones(4) * 1.0), zeros(4))
-prop2 = LinearPropagator(Diagonal(ones(4)), Diagonal(ones(4) * 1.0), zeros(4))
+prop1 = LinearPropagator(Diagonal(ones(dim)), Diagonal(ones(dim) * 1.0), zeros(dim))
+prop2 = LinearPropagator(Diagonal(ones(dim)), Diagonal(ones(dim) * 1.0), zeros(dim))
 prop_list = [prop1, prop2]
 A = [0.99 0.0;
      0.01 1.0]
-mp = ModelParameters(4, A, prop_list)
+mp = ModelParameters(dim, A, prop_list)
 
 z_ests = nothing
 log_likelis = []
@@ -30,6 +37,7 @@ for _ in 1:200 # if more than 2, usually likelihood will be static
     push!(log_likelis, log_likeli)
 end
 zs_est = [z[1] for z in z_ests]
+println("finish")
 using Plots
 plot(zs_est)
 plot!(zs)
