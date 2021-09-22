28fc455bd383d453c232b7661ea0e6a548b8dc78
HiroIshida <spitfire.docomo@gmail.com>
2021-09-23 04:24:24 +0900

    [at least] Hidden state update

--- example.jl
+++ example.jl
@@ -7,10 +7,10 @@ using StaticArrays
 using Distributions
 
 Random.seed!(0)
-states_list, phases_list = create_dataset(80)
+xs_list, phases_list = create_dataset(80)
 
 dim = 4
-xs_list = [[SVector{dim, Float64}(vcat(seq[t].x, seq[t+1].x)) for t in 1:length(seq)-1] for seq in states_list]
+#xs_list = [[SVector{dim, Float64}(vcat(seq[t].x, seq[t+1].x)) for t in 1:length(seq)-1] for seq in states_list]
 prop1 = LinearPropagator(Diagonal(ones(dim)), Diagonal(ones(dim) * 1.0), zeros(dim))
 prop2 = LinearPropagator(Diagonal(ones(dim)), Diagonal(ones(dim) * 1.0), zeros(dim))
 prop_list = [prop1, prop2]
@@ -24,13 +24,15 @@ function train!(mp, xs_list, hs_list)
         for (hs, xs) in zip(hs_list, xs_list)
             log_likeli = update_hidden_states!(hs, mp, xs)
         end
-        update_model_parameters!(hs_list, mp, xs_list)
+        #update_model_parameters!(hs_list, mp, xs_list)
     end
 end
-hs_list = [HiddenStates(length(xs), 2) for xs in xs_list]
+hs_list = [HiddenStates(xs.n_seq, 2) for xs in xs_list]
 @time train!(mp, xs_list, hs_list)
 
+#=
 using Plots
 index = 37
 plot([z[1] for z in hs_list[index].z_ests])
 plot!(phases_list[index])
+=#
--- src/ARHMM.jl
+++ src/ARHMM.jl
@@ -7,7 +7,7 @@ using Distributions
 export LinearPropagator, FixedPropagator, transition_prob
 include("propagator.jl")
 
-export HiddenStates, ModelParameters, update_hidden_states!, update_model_parameters!, Sequence
+export HiddenStates, ModelParameters, update_model_parameters!, Sequence
 
 struct Sequence{N}
     data::Matrix{Float64}
@@ -26,6 +26,7 @@ function Sequence(data::Matrix)
     n_dim, n_seq = size(data)
     Sequence{n_dim}(data, n_seq)
 end
+Base.getindex(seq::Sequence, index::Int) = seq.data[:, index]
 
 mutable struct ModelParameters{N, M}
     A::MMatrix{M, M, Float64}
@@ -121,73 +122,8 @@ function update_model_parameters!(hs::HiddenStates{M}, mp::ModelParameters{N, M}
     update_model_parameters!(hs_list, mp, xs_list)
 end
 
-function probs_linear_prop(mp::ModelParameters, x_pre, x)
-    gen = (transition_prob(prop, x_pre, x) for prop in mp.prop_list)
-end
-
-function update_hidden_states!(hs::HiddenStates{M}, mp::ModelParameters{N, M}, seq::Sequence{N}) where {N, M}
-    n_seq = length(seq)
-    alpha_forward!(hs, mp, seq)
-    beta_backward!(hs, mp, seq)
-    for t in 1:n_seq - 1
-        hs.z_ests[t] = hs.alphas[t] .* hs.betas[t]
-    end
-    n_seq = length(seq)
-
-    for t in 1:n_seq - 2
-        # i is index for t, j for (t+1)
-        for i in 1:M
-            for j in 1:M
-                x_t, x_tt = seq[t+1:t+2]
-                trans_prob = transition_prob(mp.prop_list[j], x_t, x_tt)
-                tmp = hs.alphas[t][i] * mp.A[j, i] * trans_prob * hs.betas[t+1][j]
-                hs.zz_ests[t][i, j] = tmp/hs.c_seq[t+2]
-            end
-        end
-    end
-
-    # compute log_likelihood
-    log_likeli = sum(log(c) for c in hs.c_seq)
-    return log_likeli
-end
-
-function alpha_forward!(hs::HiddenStates{M}, mp::ModelParameters{N, M}, seq::Sequence{N}) where {N, M}
-    n_seq = length(seq)
-    hs.c_seq[1] = 1.0
-
-    x1, x2 = seq[1:2]
-    px1 = 1.0 # deterministic x 
-    tmp = probs_linear_prop(mp, x1, x2) .* mp.pmf_z1 * px1
-    hs.c_seq[2] = sum(tmp)
-    hs.alphas[1] = tmp/hs.c_seq[2]
-
-    for t in 2:n_seq - 1
-        x_t, x_tp1 = seq[t:t+1]
-        for i in 1:M
-            integral_term = sum(mp.A[i, j] * hs.alphas[t-1][j] for j in 1:M)
-            hs.alphas[t][i] = transition_prob(mp.prop_list[i], x_t, x_tp1) * integral_term
-        end
-        hs.c_seq[t+1] = sum(hs.alphas[t])
-        hs.alphas[t] /= hs.c_seq[t+1]
-    end
-end
-
-function beta_backward!(hs::HiddenStates{M}, mp::ModelParameters{N, M}, seq::Sequence{N}) where {N, M}
-    n_seq = length(seq)
-    hs.betas[n_seq - 1] = ones(M)
-    for t in length(seq)-2:-1:1
-        x_tp1 = seq[t+1]
-        x_tp2 = seq[t+2]
-        for j in 1:M # phase at t
-            sum = 0.0
-            for i in 1:M # phase at t+1
-                sum +=mp.A[i, j] * transition_prob(mp.prop_list[i], x_tp1, x_tp2) * hs.betas[t+1][i]
-            end
-            hs.betas[t][j] = sum
-            hs.betas[t][j] /= hs.c_seq[t+2]
-        end
-   end
-end
+export update_hidden_states!
+include("forward_backward.jl")
 
 export create_dataset
 include("sample_dataset.jl")
