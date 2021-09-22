ac5f1de8ab17a966408118a8575deed66ed5c53e
HiroIshida <spitfire.docomo@gmail.com>
2021-09-23 06:03:17 +0900

    Clean up

--- src/maximization.jl
+++ src/maximization.jl
@@ -26,24 +26,6 @@ function update_A!(hs_list::Vector{HiddenStates{M}}, mp::ModelParameters{N, M}, 
     mp.A = A_new
 end
 
-function update_A!(hs::HiddenStates{M}, mp::ModelParameters{N, M}, x_seq) where {N, M}
-    n_seq = length(hs.z_ests) + 1
-    A_new = zeros(M, M)
-    for t in 1:n_seq - 2
-        for i in 1:M
-            for j in 1:M
-                # Note that our stochastic matrix is different (transposed) from
-                # the one in PRML
-                A_new[j, i] += hs.zz_ests[t][i, j]
-            end
-        end
-    end
-    for j in 1:M # normalize
-        A_new[:, j] /= sum(A_new[:, j])
-    end
-    mp.A = A_new
-end
-
 function update_prop_list!(hs_list::Vector{HiddenStates{M}}, mp::ModelParameters{N, M}, xs_list) where {N, M}
     for i in 1:M
         ws_list = [[z_est[i] for z_est in hs.z_ests] for hs in hs_list]
