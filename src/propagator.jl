18ee320af5a5d9368113109a4a0005af487182a4
HiroIshida <spitfire.docomo@gmail.com>
2021-09-23 03:24:45 +0900

    Refactor : avoid list complehension

--- src/propagator.jl
+++ src/propagator.jl
@@ -18,11 +18,19 @@ function LinearPropagator(phi, cov, drift)
 end
 
 function fit!(prop::LinearPropagator{N}, xs_list, ws_list) where N
-    x_sum = sum(sum(xs[t] * ws[t] for t in 1:length(xs)-1) for (xs, ws) in zip(xs_list, ws_list))
-    y_sum = sum(sum(xs[t+1] * ws[t] for t in 1:length(xs)-1) for (xs, ws) in zip(xs_list, ws_list)) 
-    xx_sum = sum(sum(xs[t] * xs[t]' * ws[t] for t in 1:length(xs)-1) for (xs, ws) in zip(xs_list, ws_list))
-    xy_sum = sum(sum(xs[t] * xs[t+1]' * ws[t] for t in 1:length(xs)-1) for (xs, ws) in zip(xs_list, ws_list))
-    w_sum = sum(sum(ws[1:end-1]) for ws in ws_list)
+    x_sum = zeros(N)
+    y_sum = zeros(N)
+    xx_sum = zeros(N, N)
+    xy_sum = zeros(N, N)
+    w_sum = 0.0
+
+    for (xs, ws) in zip(xs_list, ws_list)
+        x_sum += sum(xs[t] * ws[t] for t in 1:length(xs)-1)
+        y_sum += sum(xs[t+1] * ws[t] for t in 1:length(xs)-1)
+        xx_sum += sum(xs[t] * xs[t]' * ws[t] for t in 1:length(xs)-1)
+        xy_sum += sum(xs[t] * xs[t+1]' * ws[t] for t in 1:length(xs)-1)
+        w_sum += sum(ws[1:end-1])
+    end
 
     # Thanks to Gauss-markov theorem, we can separate fitting processes into
     # first, non probabilistic term  
